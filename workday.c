81bb37785c4730ccb797136356faa0d1c308888d
Brian Tracy <brian.tracy33@gmail.com>
2018-07-03 23:09:28 -0700

    Figuring out bytes and null terminators

--- workday.c
+++ workday.c
@@ -17,16 +17,26 @@ enum return_codes {
     ILLEGAL_RESUME_OPERATION  = 6,
     ILLEGAL_END_OPERATION     = 7,
     ILLEGAL_ARGUMENT          = 8,
-    TIME_RETRIEVE_ERROR       = 9
+    TIME_RETRIEVE_ERROR       = 9,
+    UNKNOWN_OPERATION_IN_LOG  = 10
 };
 /*
     see man 3 ctime
-    ctime returns a 26 byte string, including the newline and null terminator.
-    Thu Nov 24 18:22:48 1986\n\0
+    ctime(3) returns a 26 byte string, including the newline and null terminator.
+    workday prefixes this with two bytes, one byte for the event type and one byte for
+    a space. See the following example for a 'B'(eginning) event.
+ 
+        B Thu Nov 24 18:22:48 1986\n\0
+         2          + 26               = 28
+ 
+    So, each line in the log file will occupy 27 bytes as the null terminator is not written
+    to file.
 */
-const int BYTES_PER_LINE = 25; // see man 3 ctime
+/// excludes null terminator
+const size_t LOGFILE_BYTES_PER_LINE = 27; // excludes null terminator!
 
-char *DATA_FILE = "/Users/briantracy/Desktop/projects/workday/workday/file.txt";
+
+char *LOG_FILE = "/Users/briantracy/Desktop/projects/workday/workday/file.txt";
 
 int usage(void);
 int version(void);
@@ -38,12 +48,15 @@ int end(void);
 
 int append_line(char *);
 char *last_line(void);
+int emit_event(char event);
 
 char *current_time_str(void);
 
+
 int testmain() {
+    
     char *line = last_line();
-    printf("[%s]", line);
+    printf("[%s]\n", line);
     
     free(line);
     return 0;
@@ -51,6 +64,14 @@ int testmain() {
 
 int main(int argc, char *argv[])
 {
+    char buff[LOGFILE_BYTES_PER_LINE + 1];
+    snprintf(buff, LOGFILE_BYTES_PER_LINE, "%c %s\n", 'I', current_time_str());
+    printf("[%d]\n", buff[LOGFILE_BYTES_PER_LINE]);
+    printf("[%s]\n", buff);
+    
+    char buff1[] = { 'a', 'b', 'c', '\0', 'd', '\0' };
+    printf("[%s]\n", buff1);
+    
     return testmain();
     
     for (int i = 0; i < argc; i++) {
@@ -89,11 +110,12 @@ int main(int argc, char *argv[])
 
 struct tm parse_time_str(char *buff)
 {
-    struct tm _tm;
-    char *ret = strptime(buff, "%c", &_tm);
-    
+    struct tm time;
+    char *ret = strptime(buff, "%c", &time);
+    // first char of output from strptime is \0 on success.
+    // read the man page man 3 strptime
     if (*ret == '\0') {
-        return _tm;
+        return time;
     }
     else {
         fprintf(stderr, "[ERROR] Cannot parse the following time string: %s", buff);
@@ -108,6 +130,7 @@ struct tm *current_time(void)
     return localtime(&now);
 }
 
+/// 26 bytes including null terminator.
 char *current_time_str()
 {
     time_t now;
@@ -116,39 +139,54 @@ char *current_time_str()
 }
 
 /*
-    line is null terminated
+    line must be null terminated
 */
 int append_line(char *line)
 {
-    FILE *file = fopen(DATA_FILE, "a");
+    FILE *file = fopen(LOG_FILE, "a");
     if (file == NULL) {
-        fprintf(stderr, "[ERROR] could not open file: %s\n", DATA_FILE);
+        fprintf(stderr, "[ERROR] could not open file: %s\n", LOG_FILE);
         return FILE_WRITE_FAILURE;
     }
     
     if (fputs(line, file) < 0) {
-        fprintf(stderr, "[ERROR] could not append to file: %s\n", DATA_FILE);
+        fprintf(stderr, "[ERROR] could not append to file: %s\n", LOG_FILE);
         return FILE_WRITE_FAILURE;
     }
     return SUCCESS;
 }
 
+int emit_event(char event)
+{
+    char buff[LOGFILE_BYTES_PER_LINE + 1];
+    snprintf(buff, LOGFILE_BYTES_PER_LINE, "%c %s\n", event, current_time_str());
+    
+    return append_line(buff);
+}
+
 
 /*
     Must free the result of this call.
+    Returns NULL iff there is an error opening the file.
+    Return value is null terminated and does not include the
+    newline at the end of the line.
 */
 char *last_line(void)
 {
-    FILE *file = fopen(DATA_FILE, "r");
+    FILE *file = fopen(LOG_FILE, "r");
     if (file == NULL) {
-        fprintf(stderr, "[ERROR] could not read from file: %s", DATA_FILE);
+        fprintf(stderr, "[ERROR] could not read from file: %s\n", LOG_FILE);
+        return NULL;
     }
-    fseek(file, -BYTES_PER_LINE, SEEK_END);
-    char *buff = malloc(sizeof(char) * BYTES_PER_LINE);
-    fread(buff, BYTES_PER_LINE, 1, file);
+    fseek(file, -LOGFILE_BYTES_PER_LINE, SEEK_END);
+    char *buff = malloc(sizeof(char) * LOGFILE_BYTES_PER_LINE); // includes space for \0
+    fread(buff, LOGFILE_BYTES_PER_LINE, 1, file);
     
+    buff[LOGFILE_BYTES_PER_LINE - 1] = '\0';
     return buff;
 }
+
+
 
 int usage()
 {
@@ -166,6 +204,25 @@ int version() {
 
 int begin() {
     printf("begin\n");
+    char *last_entry = last_line();
+    if (last_entry == NULL) {
+        fprintf(stderr, "[ERROR] Aborting workday begin operation\n");
+        return FILE_READ_FAILURE;
+    }
+    
+    char op = last_entry[0];
+    switch (op) {
+        case 'B':
+        case 'P':
+        case 'R': {
+            fprintf(stderr, "[ERROR] Cannot begin workday in this state: %c\n", op);
+            return ILLEGAL_BEGIN_OPERATION;
+        }
+        case 'E': {
+            
+        }
+    }
+    
     return 0;
 }
 
